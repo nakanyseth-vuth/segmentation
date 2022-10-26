57063822d07f5e6ea2aa5c312ecc42ba0d9bd5a4
Vibol Rithiya <vibolrithiya@gmail.com>
2022-10-26 21:02:51 +0700

    Fix wrong placed sentence in different files.

--- stores/process.py
+++ stores/process.py
@@ -29,7 +29,7 @@ def segment(fileName: str):
 
     sentences = tokenize_sentences_bilstm(lines)
 
-    createDir(fileName, fileName2=fileName + "_segment")
+    createDir(fileName, fileName=fileName + "_segment")
     newFile = open("generate/{}/{}_segment.txt".format(fileName, fileName), mode="w+", encoding="UTF-8")
     for x in sentences:
         newFile.write(x + "\n")
@@ -79,47 +79,64 @@ def entity(fileName: str, intentMap: Dict[Intent, Union[List[str], List[str]]]):
         print("newSentence; " + newSentence)
         list.append(newSentence)
 
-    createDir(fileName, fileName2=fileName + "_entity")
+    createDir(fileName, fileName=fileName + "_entity")
     f = open("generate/{}/{}_entity.txt".format(fileName, fileName), mode="w+", encoding="UTF-8")
     for intent in list:
         if intent:
             f.write(intent + "\n")
     f.close()
-
     print("passeddddddd")
-    print("Error List: ")
 
 
-def createDir(fileName, fileName2):
-    os.makedirs(os.path.dirname("generate/{}/{}.txt".format(fileName, fileName2)), exist_ok=True)
+def createDir(folderName, fileName, direc: str = "generate", format: str = "txt"):
+    os.makedirs(os.path.dirname("{}/{}/{}.{}".format(direc, folderName, fileName, format)), exist_ok=True)
 
 
-def fileio(fileName, msg, mode: str = "a+"):
+def fileio(fileName, msg, folderName: str, mode: str = "a+", direc: str = "generate", format: str = "txt"):
     # create a file
-    createDir(fileName, fileName2=fileName)
-
-    f = open("generate/{}/{}.txt".format(fileName, fileName), mode=mode, encoding="utf-8")
+    createDir(folderName=folderName, fileName=fileName, format=format,direc=direc)
+    f = open("{}/{}/{}.{}".format(direc, folderName, fileName, format), mode=mode, encoding="utf-8")
     f.write(msg + "\n")
     f.close()
 
 
-def sentences(fileName, word1, word2, word3, word4, word5, initialWord: bool = True):
+def convertToCSV(fileName: str):
+    listGenerate = os.listdir("generate/{}".format(fileName))
+    listConvert = [x.split(".",maxsplit=1)[0] for x in listGenerate]
+
+    for fileConvert,fileGenerate in zip(listConvert, listGenerate):
+        fileio(fileName=fileConvert, folderName=fileName, direc="convert", mode="w", msg="Question,Intent", format="csv")
+
+        f = open("generate/{}/{}".format(fileName, fileGenerate), mode="r", encoding="UTF-8")
+        list = ["{},{}\n".format(x.strip(), fileName) for x in f.readlines()]
+        f.close()
+
+        f2 = open("convert/{}/{}.csv".format(fileName, fileConvert), mode="a", encoding="UTF-8")
+        f2.writelines(list)
+        f2.close()
+
+
+def sentences(fileName, word1, word2, word3, word4, word5, word6, initialWord: bool = True):
     # tmp = "\n word1:" + str(word1) + "\n " + "word2:" + str(word2) + "\n " + "word3:" + str(
     #     word3) + "\n " + "word4:" + str(word4) + "\n " + "word5:" + str(
     #     word5) + "\n " + "_______________________________________________"
     #
     # fileio(fileName, tmp)
-    fileio(fileName, "", mode="a+")
+    fileio(fileName, "", folderName=fileName, mode="a+")
     for i in range(len(word1)):
         for j in range(len(word2)):
             for k in range(len(word3)):
                 for m in range(len(word4)):
                     for r in range(len(word5)):
-                        x = ""
-                        if initialWord:
-                            x = "តើ" + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1] + word5[r - 1]
-                        else:
-                            x = word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1] + word5[r - 1]
-                        print(x)
-                        myList.append(x)
-                        fileio(fileName, x)
+                        for o in range(len(word6)):
+
+                            x = ""
+                            if initialWord:
+                                x = "តើ" + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1] + word5[r - 1] + \
+                                    word6[o - 1]
+                            else:
+                                x = word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1] + word5[r - 1] + word6[
+                                    o - 1]
+                            print(x)
+                            myList.append(x)
+                            fileio(fileName, x, folderName=fileName)
